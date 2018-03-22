fd4aedcdee066317655e90cc944debb638a4f816
Aorph <leo.carbonneaux@orange.fr>
2018-03-22 12:06:30 +0100

    Added security : cannot enter negatives price and stock anymore + css

--- data/validation/resources/views/insert.blade.php
+++ data/validation/resources/views/insert.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.base')
 @section('main')
   <p>Insert a New book</p>
-  {{ Form::open(['url' => '/inserted', 'method' => 'post']) }}
+  {{ Form::open(['url' => '/inserted', 'method' => 'post', 'class' => 'form']) }}
     {{ Form::label('type', 'Type') }}
-    {{ Form::text('type', 'Instrument') }}
+    {{ Form::text('type', 'Instrument', ['class' => 'input']) }}
     {{ Form::label('name', 'Name') }}
-    {{ Form::text('name', 'piano') }}
+    {{ Form::text('name', 'piano', ['class' => 'input']) }}
     {{ Form::label('description', 'Description') }}
-    {{ Form::text('description', 'A rare piano') }}
+    {{ Form::text('description', 'A rare piano', ['class' => 'input']) }}
     {{ Form::label('price', 'Price') }}
-    {{ Form::number('price', '399') }}
+    {{ Form::number('price', '399', ['class' => 'input', 'min' => '0']) }}
     {{ Form::label('stock', 'stock') }}
-    {{ Form::number('stock', '12') }}
-    {{ Form::submit('Insert') }}
+    {{ Form::number('stock', '12', ['class' => 'input', 'min' => '0']) }}
+    {{ Form::submit('Insert', ['class' => 'submit']) }}
   {{ Form::close() }}
 @endsection
--- data/validation/resources/views/update.blade.php
+++ data/validation/resources/views/update.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.base')
 @section('main')
   <p>Update item :</p>
-  {{ Form::open(['url' => '/updated', 'method' => 'post']) }}
+  {{ Form::open(['url' => '/updated', 'method' => 'post', 'class' => 'form']) }}
   {{ Form::hidden('id', $item['id']) }}
     {{ Form::label('type', 'Type') }}
-    {{ Form::text('type', $item['type']) }}
+    {{ Form::text('type', $item['type'], ['class' => 'input']) }}
     {{ Form::label('name', 'Name') }}
-    {{ Form::text('name', $item['name']) }}
+    {{ Form::text('name', $item['name'], ['class' => 'input']) }}
     {{ Form::label('description', 'Description') }}
-    {{ Form::text('description', $item['description']) }}
+    {{ Form::text('description', $item['description'], ['class' => 'input']) }}
     {{ Form::label('price', 'Price') }}
-    {{ Form::number('price', $item['price']) }}
+    {{ Form::number('price', $item['price'], ['class' => 'input', 'min' => '0']) }}
     {{ Form::label('stock', 'stock') }}
-    {{ Form::number('stock', $item['stock']) }}
-    {{ Form::submit('Update') }}
+    {{ Form::number('stock', $item['stock'], ['class' => 'input', 'min' => '0']) }}
+    {{ Form::submit('Update', ['class' => 'submit']) }}
   {{ Form::close() }}
 @endsection
